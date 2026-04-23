9b5bc3e1ddc372549a18636b5d3d17c1c3f33591
[firstname lastname] <dhamib610@gmail.com>
2026-04-23 12:50:19 +0545

    update file

--- backend/src/routes/secretRoutes.js
+++ backend/src/routes/secretRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createSecret, getSecret } from "../controllers/secretController.js";
+import {
+  createSecret,
+  getSecret,
+} from "../controllers/secretController.js";
+
 import botProtection from "../middleware/botProtection.js";
 
 const router = express.Router();
 
 router.post("/create", createSecret);
-router.get("/:token", botProtection, getSecret);
+
+
+router.post("/:token/reveal", botProtection, getSecret);
 
 export default router;

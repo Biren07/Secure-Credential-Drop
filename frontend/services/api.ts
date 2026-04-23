116a68c904beaecc3da37b1e832a66b756691068
[firstname lastname] <dhamib610@gmail.com>
2026-04-23 12:49:58 +0545

    update file

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,9 +1,10 @@
-// services/api.ts
-
 import { CreateSecretResponse, GetSecretResponse } from "../types";
 
 const BASE_URL = "https://secure-credential-drop.onrender.com";
 
+/**
+ * Create secret
+ */
 export const createSecret = async (
   text: string,
   ttl: number
@@ -19,9 +20,21 @@ export const createSecret = async (
   return res.json();
 };
 
+/**
+ * Reveal secret (FIXED)
+ */
 export const getSecret = async (
   token: string
 ): Promise<GetSecretResponse> => {
-  const res = await fetch(`${BASE_URL}/secret/${token}`);
+  const res = await fetch(
+    `${BASE_URL}/secret/${token}/reveal`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
   return res.json();
 };
